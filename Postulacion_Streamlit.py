aae7fddb6ddcf5aa003bd1665af1ea662d031f72
Agusttn07 <agustinmoraga2007@gmail.com>
2025-09-11 18:31:52 -0300

    Update Postulacion_Streamlit.py

--- Postulacion_Streamlit.py
+++ Postulacion_Streamlit.py
@@ -30,7 +30,7 @@ def clamp_0_1000(x: Optional[str]) -> Optional[float]:
 def cargar_ponderaciones(force_update=False):
     data = [
         #Universidad de Chile
-        {"universidad": "Universidad de Chile", "carrera": "Arquitectura, "sede":"Beauchef","NEM": 10, "Ranking": 20, "Lectora": 20, "M1": 30, "M2": 10, "Ciencias": 10, "Historia": 10, "Corte": 746.80},
+        {"universidad":"Universidad de Chile,"carrera":"Arquitectura","sede":"Beauchef","NEM":10,"Ranking":20,"Lectora":20,"M1":30,"M2":10,"Ciencias":10,"Historia":10,"Corte":746.80},
          
 
         #Universidad Catolica
